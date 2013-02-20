c57d111d72bb57bcce33aa917c5ad9a0b15c6312
jas- <jason.gerfen@gmail.com>
2013-02-20 11:04:19 -0700

    Wed Feb 20 11:04:19 MST 2013 - Added some inline code commenting for easier maintenance

--- js/current-inventory.js
+++ js/current-inventory.js
@@ -1,7 +1,14 @@
 $(document).ready(function(){
 
+	/* Unload current grid on collapse */
+	$("#current-inventory").bind('collapse', function(event, ui) {
+		//$("#jqxgrid").html(false);
+	});
+
+	/* On expand load current inventory from server, generate grid & populate */
 	$("#current-inventory").bind('expand', function(event, ui) {
 
+		/* Query server for JSON formatted inventory data */
    	$('#current-inventory').offline({
    	  appID:'MLIB-Inventory',
    	  url: 'http://new-inventory.scl.utah.edu/?do=current',
@@ -11,9 +18,11 @@ $(document).ready(function(){
    	  }
    	});
   
+		/* Handle grid init, options, sorting, paging & editing within grid */
 		function _display(obj){
    	  var theme = getDemoTheme();
   
+			/* Here we handle sorting of all columns */
    	  var customsortfunc = function (column, direction) {
 				var sortdata = new Array();
 
@@ -38,48 +47,52 @@ $(document).ready(function(){
 				}
 				source.localdata = sortdata;
 				$("#jqxgrid").jqxGrid('databind', source, 'sort');
-				$("#jqxgrid").jqxGrid('savestate');
+				//$("#jqxgrid").jqxGrid('savestate');
 				Object.prototype.toString = tmpToString;
 		  };
 
+			/* Here we perform comparision of strings */
 		  var compare = function (value1, value2) {
-			value1 = String(value1).toLowerCase();
-			value2 = String(value2).toLowerCase();
+				value1 = String(value1).toLowerCase();
+				value2 = String(value2).toLowerCase();
 
-			try {
-			  var tmpvalue1 = parseFloat(value1);
-			  if (isNaN(tmpvalue1)) {
-					if (value1 < value2) { return -1; }
-					if (value1 > value2) { return 1; }
-			  } else {
-					var tmpvalue2 = parseFloat(value2);
-					if (tmpvalue1 < tmpvalue2) { return -1; }
-					if (tmpvalue1 > tmpvalue2) { return 1; }
-			  }
-			} catch (error) {
-			  var er = error;
-			}
-			return 0;
-	  };
+				try {
+				  var tmpvalue1 = parseFloat(value1);
+				  if (isNaN(tmpvalue1)) {
+						if (value1 < value2) { return -1; }
+						if (value1 > value2) { return 1; }
+				  } else {
+						var tmpvalue2 = parseFloat(value2);
+						if (tmpvalue1 < tmpvalue2) { return -1; }
+						if (tmpvalue1 > tmpvalue2) { return 1; }
+				  }
+				} catch (error) {
+				  var er = error;
+				}
+				return 0;
+		  };
 
-	  $('#export-csv').on('click', function(){
-			$("#jqxgrid").jqxGrid('exportdata', 'csv', _date()+'-MLIB-Inventory');
-	  });
-	  $('#export-pdf').on('click', function(){
-				$("#jqxgrid").jqxGrid('exportdata', 'pdf', _date()+'-MLIB-Inventory');
-	  });
-	  $('#export-xls').on('click', function(){
-			$("#jqxgrid").jqxGrid('exportdata', 'xls', _date()+'-MLIB-Inventory');
-	  });
+			/* Create some export options */
+		  $('#export-csv').on('click', function(){
+				$("#jqxgrid").jqxGrid('exportdata', 'csv', _date()+'-MLIB-Inventory');
+		  });
+		  $('#export-pdf').on('click', function(){
+					$("#jqxgrid").jqxGrid('exportdata', 'pdf', _date()+'-MLIB-Inventory');
+		  });
+		  $('#export-xls').on('click', function(){
+				$("#jqxgrid").jqxGrid('exportdata', 'xls', _date()+'-MLIB-Inventory');
+		  });
+		
+			/* When row count changes save state */
+		  $("#jqxgrid").on("pagesizechanged", function (event) {
+				//$("#jqxgrid").jqxGrid('savestate');
+		  });
 	
-	  $("#jqxgrid").on("pagesizechanged", function (event) {
-			$("#jqxgrid").jqxGrid('savestate');
-	  });
-	
-	  var source = {
-			localdata: obj,
-			sort: customsortfunc,
-			datafields:[
+			/* Map our JSON object to fields */
+		  var source = {
+				localdata: obj,
+				sort: customsortfunc,
+				datafields:[
 				  { name: 'Computer', type: 'string' },
 				  { name: 'SKU', type: 'string' },
 				  { name: 'Serial', type: 'string' },
@@ -90,13 +103,15 @@ $(document).ready(function(){
 				datatype: "json"
 		  };
 		  var dataAdapter = new $.jqx.dataAdapter(source);
-
+	
+			/* Handle editing of record elements */
 		  $("#jqxgrid").on('cellendedit', function (event) {
 				var args = event.args;
 				//$("#cellendeditevent").text("Event Type: cellendedit, Column: " + args.datafield + ", Row: " + (1 + args.rowindex) + ", Value: " + args.value);
 			    // call offline to send edited row contents
 		  });
 
+			/* Initialize grid with options while binding events etc */
 		  $("#jqxgrid").jqxGrid({
 				autoshowloadelement: true,
 				width: '100%',
@@ -112,7 +127,7 @@ $(document).ready(function(){
 				editable: true,
 				selectionmode: 'multiplecellsadvanced',
 				ready: function () {
-				  $("#jqxgrid").jqxGrid('loadstate', $("#jqxgrid").jqxGrid('getstate'));
+				  //$("#jqxgrid").jqxGrid('loadstate', $("#jqxgrid").jqxGrid('getstate'));
 				  $("#jqxgrid").jqxGrid('sortby', 'Hostname', 'asc');
 				},
 				columns: [
@@ -126,6 +141,7 @@ $(document).ready(function(){
 		  });
 		}
 
+		/* Helper function to inspect objects recursively */
 		function _inspect(obj){
 		  $.each(obj, function(x, y){
 			if ((/object|array/.test(typeof(y))) && (_size(y) > 0)){
@@ -137,6 +153,7 @@ $(document).ready(function(){
 		  });
 		}
 
+		/* Calculate size of an object */
 		function _size(obj){
 		  var n = 0;
 		  if (/object/.test(typeof(obj))) {
@@ -149,6 +166,7 @@ $(document).ready(function(){
 		  return n;
 		}
 
+		/* Return an ISO formatted date */
 		function _date(){
 		  var _d = new Date();
 		  return _d.toISOString();
