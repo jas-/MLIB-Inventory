347f968d4f80b6b6dc2f03d72792c5acf93f871a
jas- <jason.gerfen@gmail.com>
2013-02-28 07:54:02 -0700

    Thu Feb 28 07:54:01 MST 2013 - Hope I didn't loose some changes

--- js/inventory.js
+++ js/inventory.js
@@ -1,290 +1,267 @@
 $(document).ready(function(){
 
-	$("#main").live('pagebeforecreate',function(event){
+	$("#main #search #add").live('pagebeforecreate',function(event){
 		return false;
 	});
+
+	($('.ui-page-active').attr('id')=='main') ? _load('current', 'http://new-inventory.scl.utah.edu/?do=current', {'do':true}, false, true) : false;
+
+	($('.ui-page-active').attr('id')=='search') ? _load('search', false, false, true, true) : false;
 
 	$("#main").live('pagecreate pageshow', function(event, ui) {
-		$("#jqxgrid").jqxGrid('destroy');
-		$("#jqxWidget").html('<div id="jqxgrid"></div>');
-		_load();
-	});
-
-	($('.ui-page-active').attr('id')=='main') ? _load_main() : false;
-
-	/* retrieve current inventory, setup handlers, options & render grid */
-	function _load_main(){
-
-		/* Query server for JSON formatted inventory data */
-		$('#current-inventory').offline({
-			appID:'MLIB-Inventory',
-			url: 'http://new-inventory.scl.utah.edu/?do=current',
-			data: {'do':true},
-			callback: function(){
-				_display($(this));
-			}
-		});
-	}
-
-	$("#search").live('pagebeforecreate',function(event){
-		return false;
+		_destroy('current');
+		_load('current', 'http://new-inventory.scl.utah.edu/?do=current', {'do':true}, false, true);
 	});
 
 	$("#search").live('pagecreate pageshow',function(event){
-		_load();
-	});
-
-	($('.ui-page-active').attr('id')=='search') ? _load_search() : false;
-
-	function _load_search(){
-		_destroy('jqxgrid-search');
-		$('#search-computer').offline({
-			appID:'MLIB-Inventory',
-			debug: true,
-			callback: function(){
-				_destroy('jqxgrid-search');
-				_display($(this));
-			}
-		});
-	}
-
-	$("#add").live('pagebeforecreate',function(event){
-		return false;
+		_load('search', false, false, true, true);
 	});
 
 	$("#add").live('pagecreate pageshow',function(event){
-		$('#add-computer').offline({
-			appID:'MLIB-Inventory',
-			callback: function(){
-				_message($(this));
-			}
-		});
+		_load('add-computer', false, false, false, false);
 	});
 
-	/* Destroy grid element */
 	function _destroy(ele){
-		$("#"+ele).jqxGrid('destroy');
-		$("#"+ele).html('<div id="'+ele+'"></div>');
+		$("#jqxgrid-"+ele).jqxGrid('destroy');
+		$("#jqxWidget-"+ele).html('<div id="jqxgrid-'+ele+'"></div>');
 	}
 
-	/* Handle grid init, options, sorting, paging & editing within grid */
-	function _display(obj){
+	function _load(ele, url, data, destroy, grid){
+		$('#'+ele).comm({
+			debug: true,
+			appID:'MLIB-Inventory',
+			url: (url) ? url : false,
+			data: (data) ? data : false,
+			callback: function(){
+				(destroy) ? _destroy(ele) : false;
+				(grid) ? _display($(this), ele) : false;
+				_message($(this), ele);
+			}
+		});
+	}
+
+	function _display(obj, ele){
 
 		var theme = getDemoTheme();
 
 		/* Here we handle sorting of all columns */
 		var customsortfunc = function (column, direction) {
-		var sortdata = new Array();
+			var sortdata = new Array();
 
-		if (direction == 'ascending') direction = true;
-		if (direction == 'descending') direction = false;
+			if (direction == 'ascending') direction = true;
+			if (direction == 'descending') direction = false;
 
-		if (direction != null) {
-			for (i = 0; i < obj.length; i++) {
-				sortdata.push(obj[i]);
-			}
-		} else {
-			sortdata = obj;
-		}
-
-		var tmpToString = Object.prototype.toString;
-		Object.prototype.toString = (typeof column == "function") ? column : function () { return this[column] };
-		if (direction != null) {
-			sortdata.sort(compare);
-			if (!direction) {
-				sortdata.reverse();
-			}
-		}
-		source.localdata = sortdata;
-		$("#jqxgrid-search").jqxGrid('databind', source, 'sort');
-		//$("#jqxgrid-search").jqxGrid('savestate');
-		Object.prototype.toString = tmpToString;
-	};
-
-	/* Here we perform comparision of strings */
-	var compare = function (value1, value2) {
-		value1 = String(value1).toLowerCase();
-		value2 = String(value2).toLowerCase();
-
-		try {
-			var tmpvalue1 = parseFloat(value1);
-			if (isNaN(tmpvalue1)) {
-				if (value1 < value2) { return -1; }
-				if (value1 > value2) { return 1; }
+			if (direction != null) {
+				for (i = 0; i < obj.length; i++) {
+					sortdata.push(obj[i]);
+				}
 			} else {
-				var tmpvalue2 = parseFloat(value2);
-				if (tmpvalue1 < tmpvalue2) { return -1; }
-				if (tmpvalue1 > tmpvalue2) { return 1; }
+				sortdata = obj;
 			}
-		} catch (error) {
-			var er = error;
-		}
-		return 0;
-	};
 
-	/* Map our JSON object to fields */
-	var source = {
-		localdata: obj,
-		sort: customsortfunc,
-		datafields:[
-			{ name: 'Computer', type: 'string' },
-			{ name: 'SKU', type: 'string' },
-			{ name: 'Serial', type: 'string' },
-			{ name: 'UUIC', type: 'string' },
-			{ name: 'MSerial', map: 'Monitor>0>MSerial', type: 'string' },
-			{ name: 'MSKU', map: 'Monitor>0>MSKU', type: 'string' }
-		],
-		datatype: "json"
-	};
-	var dataAdapter = new $.jqx.dataAdapter(source);
+			var tmpToString = Object.prototype.toString;
+			Object.prototype.toString = (typeof column == "function") ? column : function () { return this[column] };
+			if (direction != null) {
+				sortdata.sort(compare);
+				if (!direction) {
+					sortdata.reverse();
+				}
+			}
+			source.localdata = sortdata;
+			$("#jqxgrid-"+ele).jqxGrid('databind', source, 'sort');
+			//$("#jqxgrid").jqxGrid('savestate');
+			Object.prototype.toString = tmpToString;
+		};
 
-	/* Handle editing of record elements */
-	$("#jqxgrid-search").on({
-		cellvaluechanged: function(event){
-			var _d = $('#jqxgrid').jqxGrid('getrowdata', args.rowindex);
+		/* Here we perform comparision of strings */
+		var compare = function (value1, value2) {
+			value1 = String(value1).toLowerCase();
+			value2 = String(value2).toLowerCase();
 
-			var _c = '{"hostname":"'+_d.Computer+'","sku":"'+_d.SKU+'","uuic":"'+_d.UUIC+'","serial":"'+_d.Serial+'"}';
-			var _m = '{"hostname":"'+_d.Computer+'","sku":"'+_d.MSKU+'","serial":"'+_d.MSerial+'"}';
+			try {
+				var tmpvalue1 = parseFloat(value1);
+				if (isNaN(tmpvalue1)) {
+					if (value1 < value2) { return -1; }
+					if (value1 > value2) { return 1; }
+				} else {
+					var tmpvalue2 = parseFloat(value2);
+					if (tmpvalue1 < tmpvalue2) { return -1; }
+					if (tmpvalue1 > tmpvalue2) { return 1; }
+				}
+			} catch (error) {
+				var er = error;
+			}
+			return 0;
+		};
 
-			$('#current-inventory').offline({
+		/* Map our JSON object to fields */
+		var source = {
+			localdata: obj,
+			sort: customsortfunc,
+			datafields:[
+				{ name: 'Computer', type: 'string' },
+				{ name: 'SKU', type: 'string' },
+				{ name: 'Serial', type: 'string' },
+				{ name: 'UUIC', type: 'string' },
+				{ name: 'MSerial', map: 'Monitor>0>Serial', type: 'string' },
+				{ name: 'MSKU', map: 'Monitor>0>SKU', type: 'string' }
+			],
+			datatype: "json"
+		};
+		var dataAdapter = new $.jqx.dataAdapter(source);
+
+		/* Handle editing of record elements */
+		$("#jqxgrid-"+ele).on({
+			cellvaluechanged: function(event){
+				var _d = $('#jqxgrid-'+ele).jqxGrid('getrowdata', args.rowindex);
+
+				var _c = '{"hostname":"'+_d.Computer+'","sku":"'+_d.SKU+'","uuic":"'+_d.UUIC+'","serial":"'+_d.Serial+'"}';
+				var _m = '{"hostname":"'+_d.Computer+'","sku":"'+_d.MSKU+'","serial":"'+_d.MSerial+'"}';
+
+				$('#current-inventory').comm({
 				appID:'MLIB-Inventory',
 				url: 'http://new-inventory.scl.utah.edu/?do=add',
-				data: _c,
-				callback: function(){
-					_message($(this));
-				}
-			});
+					data: _c,
+					callback: function(){
+						_message($(this));
+					}
+				});
 
-			$('#current-inventory').offline({
-				appID:'MLIB-Inventory',
-				url: 'http://new-inventory.scl.utah.edu/?do=add-monitor',
-				data: _m,
-				callback: function(){
-					_message($(this));
-				}
-			});
-		}
-	});
+				$('#current-inventory').comm({
+					appID:'MLIB-Inventory',
+					url: 'http://new-inventory.scl.utah.edu/?do=add-monitor',
+					data: _m,
+					callback: function(){
+						_message($(this));
+					}
+				});
+			}
+		});
 
-	/* Create some export options */
-	$('#export-csv').on('click', function(){
-		$("#jqxgrid-search").jqxGrid('exportdata', 'csv', _date()+'-MLIB-Inventory');
-	});
-	$('#export-pdf').on('click', function(){
-		$("#jqxgrid-search").jqxGrid('exportdata', 'pdf', _date()+'-MLIB-Inventory');
-	});
-	$('#export-xls').on('click', function(){
-		$("#jqxgrid-search").jqxGrid('exportdata', 'xls', _date()+'-MLIB-Inventory');
-	});
+		/* Create some export options */
+		$('#export-csv').on('click', function(){
+			$("#jqxgrid").jqxGrid('exportdata', 'csv', _date()+'-MLIB-Inventory');
+		});
+		$('#export-pdf').on('click', function(){
+			$("#jqxgrid").jqxGrid('exportdata', 'pdf', _date()+'-MLIB-Inventory');
+		});
+		$('#export-xls').on('click', function(){
+			$("#jqxgrid").jqxGrid('exportdata', 'xls', _date()+'-MLIB-Inventory');
+		});
 
-	/* When row count changes save state */
-	$("#jqxgrid-search").on("pagesizechanged", function (event) {
-		//$("#jqxgrid-search").jqxGrid('savestate');
-	});
+		/* Create some filter options */
 
-	if (_detect()){
-		/* handle the pager (since the default can't be adjusted) */
-		var pagerrenderer = function () {
-			var element = $("<div style='margin-top: 5px; width: 100%; height: 100%;'></div>");
-			var datainfo = $("#jqxgrid-search").jqxGrid('getdatainformation');
-			var paginginfo = datainfo.paginginformation;
+		/* When row count changes save state */
+		$("#jqxgrid-"+ele).on("pagesizechanged", function (event) {
+			//$("#jqxgrid").jqxGrid('savestate');
+		});
 
-			// create navigation buttons.
-			var leftButton = $("<div style='padding: 1px; float: left;'><div style='margin-left: 9px; width: 16px; height: 16px;'></div></div>");
-			leftButton.find('div').addClass('icon-arrow-left');
-			leftButton.width(36);
-			leftButton.jqxButton();
-
-			var rightButton = $("<div style='padding: 1px; margin: 0px 3px; float: left;'><div style='margin-left: 9px; width: 16px; height: 16px;'></div></div>");
-			rightButton.find('div').addClass('icon-arrow-right');
-			rightButton.width(36);
-			rightButton.jqxButton();
-
-			// append the navigation buttons to the container DIV tag.
-			leftButton.appendTo(element);
-			rightButton.appendTo(element);
-
-			// create a page information label and append it to the container DIV tag.
-			var label = $("<div style='font-size: 11px; margin: 2px 3px; font-weight: bold; float: left;'></div>");
-			label.text("1-" + paginginfo.pagesize + ' of ' + datainfo.rowscount);
-			label.appendTo(element);
-
-			// navigate to the next page when the right navigation button is clicked.
-			rightButton.click(function () {
-				$("#jqxgrid-search").jqxGrid('gotonextpage');
-				var datainfo = $("#jqxgrid-search").jqxGrid('getdatainformation');
+		if (_detect()){
+			/* handle the pager (since the default can't be adjusted) */
+			var pagerrenderer = function () {
+				var element = $("<div style='margin-top: 5px; width: 100%; height: 100%;'></div>");
+				var datainfo = $("#jqxgrid-"+ele).jqxGrid('getdatainformation');
 				var paginginfo = datainfo.paginginformation;
-				label.text(1 + paginginfo.pagenum * paginginfo.pagesize + "-" + Math.min(datainfo.rowscount, (paginginfo.pagenum + 1) * paginginfo.pagesize) + ' of ' + datainfo.rowscount);
-			});
 
-			// navigate to the previous page when the right navigation button is clicked.
-			leftButton.click(function () {
-				$("#jqxgrid-search").jqxGrid('gotoprevpage');
-				var datainfo = $("#jqxgrid-search").jqxGrid('getdatainformation');
-				var paginginfo = datainfo.paginginformation;
-				label.text(1 + paginginfo.pagenum * paginginfo.pagesize + "-" + Math.min(datainfo.rowscount, (paginginfo.pagenum + 1) * paginginfo.pagesize) + ' of ' + datainfo.rowscount);
-			});
-			return element;
+				// create navigation buttons.
+				var leftButton = $("<div style='padding: 1px; float: left;'><div style='margin-left: 9px; width: 16px; height: 16px;'></div></div>");
+				leftButton.find('div').addClass('icon-arrow-left');
+				leftButton.width(36);
+				leftButton.jqxButton();
+
+				var rightButton = $("<div style='padding: 1px; margin: 0px 3px; float: left;'><div style='margin-left: 9px; width: 16px; height: 16px;'></div></div>");
+				rightButton.find('div').addClass('icon-arrow-right');
+				rightButton.width(36);
+				rightButton.jqxButton();
+
+				// append the navigation buttons to the container DIV tag.
+				leftButton.appendTo(element);
+				rightButton.appendTo(element);
+
+				// create a page information label and append it to the container DIV tag.
+				var label = $("<div style='font-size: 11px; margin: 2px 3px; font-weight: bold; float: left;'></div>");
+				label.text("1-" + paginginfo.pagesize + ' of ' + datainfo.rowscount);
+				label.appendTo(element);
+
+				// navigate to the next page when the right navigation button is clicked.
+				rightButton.click(function () {
+					$("#jqxgrid").jqxGrid('gotonextpage');
+					var datainfo = $("#jqxgrid-"+ele).jqxGrid('getdatainformation');
+					var paginginfo = datainfo.paginginformation;
+					label.text(1 + paginginfo.pagenum * paginginfo.pagesize + "-" + Math.min(datainfo.rowscount, (paginginfo.pagenum + 1) * paginginfo.pagesize) + ' of ' + datainfo.rowscount);
+				});
+
+				// navigate to the previous page when the right navigation button is clicked.
+				leftButton.click(function () {
+					$("#jqxgrid-"+ele).jqxGrid('gotoprevpage');
+					var datainfo = $("#jqxgrid").jqxGrid('getdatainformation');
+					var paginginfo = datainfo.paginginformation;
+					label.text(1 + paginginfo.pagenum * paginginfo.pagesize + "-" + Math.min(datainfo.rowscount, (paginginfo.pagenum + 1) * paginginfo.pagesize) + ' of ' + datainfo.rowscount);
+				});
+				return element;
+			}
+		} else {
+			var pagerrenderer = false;
 		}
-	} else {
-		var pagerrenderer = false;
-	}
 
-	/* Initialize grid with options while binding events etc */
-	$("#jqxgrid-search").jqxGrid({
-		autoshowloadelement: true,
-		width: '100%',
-		altrows: true,
-		pagerrenderer: pagerrenderer,
-		pagesize: (_detect())?5:20,
-		pagesizeoptions: ['5', '10', '20', '30', '40', '50'],
-		source: dataAdapter,
-		theme: theme,
-		sortable: true,
-		pageable: true,
-		autoheight: true,
-		editable: true,
-		enabletooltips: true,
-		selectionmode: 'multiplecellsadvanced',
-		ready: function () {
-			//$("#jqxgrid-search").jqxGrid('loadstate', $("#jqxgrid-search").jqxGrid('getstate'));
-			$("#jqxgrid-search").jqxGrid('sortby', 'Hostname', 'asc');
-		},
-		columns: [
-			{ text: 'Hostname', datafield: 'Computer', width: '20%' },
-			{ text: 'SKU', datafield: 'SKU', width: '20%' },
-			{ text: 'Serial', datafield: 'Serial', width: '20%' },
-			{ text: 'UUIC', datafield: 'UUIC', width: '10%' },
-			{ text: 'Monitor Serial', datafield: 'MSerial', width: '20%' },
-			{ text: 'Monitor SKU', datafield: 'MSKU', width: '10%' }
+		/* Initialize grid with options while binding events etc */
+		$("#jqxgrid-"+ele).jqxGrid({
+			autoshowloadelement: true,
+			width: '100%',
+			altrows: true,
+			pagerrenderer: pagerrenderer,
+			pagesize: (_detect())?5:20,
+			pagesizeoptions: ['5', '10', '20', '30', '40', '50'],
+			source: dataAdapter,
+			theme: theme,
+			sortable: true,
+			pageable: true,
+			autoheight: true,
+			editable: true,
+			enabletooltips: true,
+			autosave: true,
+			autorestore: true,
+			selectionmode: 'multiplecellsadvanced',
+			ready: function () {
+				//$("#jqxgrid").jqxGrid('loadstate', $("#jqxgrid").jqxGrid('getstate'));
+				$("#jqxgrid-"+ele).jqxGrid('sortby', 'Hostname', 'asc');
+			},
+			columns: [
+				{ text: 'Hostname', datafield: 'Computer', width: '20%' },
+				{ text: 'SKU', datafield: 'SKU', width: '20%' },
+				{ text: 'Serial', datafield: 'Serial', width: '20%' },
+				{ text: 'UUIC', datafield: 'UUIC', width: '10%' },
+				{ text: 'Monitor Serial', datafield: 'MSerial', width: '20%' },
+				{ text: 'Monitor SKU', datafield: 'MSKU', width: '10%' }
 			]
 		});
 	}
 
-	function _message(obj){
+	function _message(obj, ele){
 		if (obj!='') {
 			$.each(obj, function(key,value){
 				$.each(value, function(k,v){
 					if(k=='error'){
-						$('#message').html('<div class="error">'+v+'</div>').fadeIn(1000);
+						$('#message-'+ele).html('<div class="error">'+v+'</div>').fadeIn(1000);
 					}
 					if(k=='warning'){
-						$('#message').html('<div class="warning">'+v+'</div>').fadeIn(1000);
+						$('#message-'+ele).html('<div class="warning">'+v+'</div>').fadeIn(1000);
 					}
 					if(k=='info'){
-						$('#message').html('<div class="info">'+v+'</div>').fadeIn(1000);
+						$('#message-'+ele).html('<div class="info">'+v+'</div>').fadeIn(1000);
 					}
 					if(k=='success'){
-						$('#message').html('<div class="success">'+v+'</div>').fadeIn(1000);
+						$('#message-'+ele).html('<div class="success">'+v+'</div>').fadeIn(1000);
 					}
 				});
 			});
 		} else {
-			$('#message').html('<div class="warning">Empty response for request</div>').fadeIn(1000);
+			$('#message-'+ele).html('<div class="warning">Empty response for request</div>').fadeIn(1000);
 		}
+		setTimeout(function() {
+			$('#message'+ele).fadeOut();
+		}, 2000);
 	}
-
-	setTimeout(function() {	$('#message').fadeOut(); }, 2000);
 
 	/* Helper function to inspect objects recursively */
 	function _inspect(obj){
