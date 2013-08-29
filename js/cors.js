5d8a29d5172b1a98a13bdfd0a41e81cfdb223e49
jas- <jason.gerfen@gmail.com>
2013-08-29 07:59:21 -0600

    Thu Aug 29 07:59:21 MDT 2013
    UI:
      Added individual components for each management section (IE monitors, rma, cors, etc).

--- js/cors.js
+++ js/cors.js
@@ -1,18 +1,47 @@
 $(document).ready(function(){
-	/* Mappings for cors data source */
+
+	/* DOM element storing dqxGrid */
+	var grid = 'jqxgrid-cors';
+
+	/* Name of client storage key */
+	var key = 'cors';
+
+  /* Mappings for computer data source */
 	var cors = {
 		source: {
 			datafields:[
-				{ name:		'Application',type: 'string' },
-				{ name:		'URL',				type: 'string' },
-				{ name:		'IP',					type: 'string' },
+				{ name:		'Application',	type: 'string' },
+				{ name:		'URL',					type: 'string' },
+				{ name:		'IP',						type: 'string' }
 			],
-			datatype: "json"
+			datatype: 'json'
 		},
 		columns: [
-			{ text: 'Application',datafield: 'Application',	width: '20%' },
-			{ text: 'URL',				datafield: 'URL',					width: '20%' },
-			{ text: 'IP',					datafield: 'IP',					width: '10%' }
+			{ text: 'Application',
+        datafield: 'Application',
+        width: '36%',
+        validation: function (cell, value) {
+          return valGeneral(value);
+        }
+      },
+			{ text: 'URL',
+        datafield: 'URL',
+        width: '32%',
+        validation: function(cell, value) {
+          return valURL(value);
+        }
+      },
+			{ text: 'IP',
+        datafield: 'IP',
+        width: '32%',
+        validation: function(cell, value) {
+          return valIP(value);
+        }
+      },
 		]
 	};
+
+  /* Initialize computer record set */
+	doIt(key, grid, api.cors.url, methods.all, cors)
+
 });
